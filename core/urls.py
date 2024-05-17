e65ced883d0f14a451b8301b8e344352ec1152b1
Wasim Ranjha <wasimranjhaa@gmail.com>
2024-05-17 23:48:32 +0500

    Initial commit of Django project

---
+++ core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import home, youtube_view, create_todo, delete_todo, todo, books, dictionary_view, wikipedia_view, notes, note_detail, homework, delete_homework, delete_note, conversion, contact_us
+
+urlpatterns = [
+    path('', home, name='home'),
+    path("books", books, name="books"),
+    path("contact", contact_us, name="contactus"),
+    path("dict", dictionary_view, name="dictionary"),
+    path('youtube-search', youtube_view, name='youtube-search'),
+    path('todo', todo, name='todo'),
+    path('create-todo', create_todo, name='create-todo'),
+    path('conversation', conversion, name='conversation'),
+    path('delete-todo/<int:todo_id>', delete_todo, name='delete-todo'),
+    path('notes', notes, name='notes'),
+    path('notes-detail/<int:pk>', note_detail, name='notedetail'),
+    path('notes-delete/<int:pk>', delete_note, name='notedelete'),
+    path("wiki", wikipedia_view, name="wikipedia"),
+    path('homework/', homework, name='homework'),
+    path('homework/delete/<int:homework_id>/', delete_homework, name='delete_homework'),
+]
