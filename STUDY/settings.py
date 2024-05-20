a2f61e4f09bf1433b39918ddd145b9d260fc320e
Wasim Ranjha <wasimranjhaa@gmail.com>
2024-05-21 01:50:47 +0500

    Code Updated

--- STUDY/settings.py
+++ STUDY/settings.py
@@ -1,7 +1,5 @@
-
-
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -67,11 +65,11 @@ WSGI_APPLICATION = 'STUDY.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studyportal',
-        'USER': 'studyportaladmin',
-        'PASSWORD': 'admin1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
